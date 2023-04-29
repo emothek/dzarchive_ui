3e70cdd6d72b47969ab7264cf0a694cb7aa858e3
emothek <mokhtar.developer@gmail.com>
2023-04-29 22:13:08 +0100

    add prefetchMethod to doc viewer

--- src/components/File.js
+++ src/components/File.js
@@ -11,6 +11,7 @@ export default function File({ file }) {
   return (
     <div>
       <DocViewer
+        prefetchMethod="GET"
         pluginRenderers={DocViewerRenderers}
         documents={selectedDocs.map((f) => {
           return {
